37056187520c97eefdf2e14bbef359dbfffec027
Issam Benamara <issamou.b@hotmail.fr>
2019-10-07 03:41:10 +0200

    Update bow.py

--- bow.py
+++ bow.py
@@ -3,8 +3,12 @@
 def compute_feats(vdict, image_sifts):
     from scipy.spatial import distance_matrix
     # TODO compute BoW from `image_sifts`
+    sift = image_sifts
+    sift = [s.reshape(-1, sift[0].shape[-1]) for s in sift]
+    sift = np.concatenate(sift, axis=0)
+    
     # Coding
-    dm = distance_matrix(image_sifts,vidct)
+    dm = distance_matrix(sift,vidct)
     ids = np.argmin(dm,axis=1)
     h = np.zeros(dm.shape)
     h[range(len(ids)),ids] = 1
